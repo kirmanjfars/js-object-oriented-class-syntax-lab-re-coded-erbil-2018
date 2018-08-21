6a846bb85a5e4d0bcc1a1a589f4ca2ab1e78e564
Kirmanj Faris <kirmanj.00806510@gmail.com>
2018-08-21 20:15:31 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -38,19 +38,19 @@ function Ceo(name, homeState, training){
     return 'No, I must disagree'
   }
   
-  BoardMember.prototype.approve = function(){
+  Ceo.prototype.approve = function(){
     return 'You can do that!'
   }
   
-  BoardMember.prototype.doCharity = function(){
+  Ceo.prototype.doCharity = function(){
     return "I like to help people."
   }
 
-  BoardMember.prototype.releasePressStatement = function(){
+  Ceo.prototype.releasePressStatement = function(){
     return "You will see great things from Scuber."
   }
 
-  BoardMember.prototype.sayHi = function(){
+  Ceo.prototype.sayHi = function(){
     return `Hi, my name is ${this.name}. I am from ${this.homeState}, and I was trained in ${this.training}.`
   }
   
