d98b1b6d38ba2f197331332b02ef4ddb8edcd052
Kirmanj Faris <kirmanj.00806510@gmail.com>
2018-08-21 20:15:35 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -54,6 +54,5 @@ function Ceo(name, homeState, training){
     return `Hi, my name is ${this.name}. I am from ${this.homeState}, and I was trained in ${this.training}.`
   }
   
-  ceo.hireEmployee()
-  
+
 }
